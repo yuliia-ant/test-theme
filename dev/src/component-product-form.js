8d571c1e45af4b6bf8f57f2af9b5170bf75797fb
yuliia-ant <yuliia@refactorplus.com>
2023-12-21 10:16:28 +0200

    fixed code style & naming

--- dev/src/component-product-form.js
+++ dev/src/component-product-form.js
@@ -8,14 +8,7 @@ if (!customElements.get("custom-form")) {
         this.form = this.querySelector("form");
         this.form.querySelector("[name=id]").disabled = false;
         this.form.addEventListener("submit", this.onSubmitHandler.bind(this));
-        this.cart =
-          document.querySelector("cart-notification") ||
-          document.querySelector("cart-drawer");
         this.submitButton = this.querySelector('[type="submit"]');
-
-        // if (document.querySelector('cart-drawer')) this.submitButton.setAttribute('aria-haspopup', 'dialog');
-
-        // this.hideErrors = this.dataset.hideErrors === 'true';
       }
 
       onSubmitHandler(evt) {
@@ -26,7 +19,6 @@ if (!customElements.get("custom-form")) {
         );
 
         this.submitButton.setAttribute("aria-disabled", "true");
-        // this.submitButton.setAttribute('disabled', 'true');
 
         this.querySelector(".loading__spinner").classList.remove("hidden");
 
